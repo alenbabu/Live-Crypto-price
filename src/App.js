c64f507f1b85d9da9b91f7b5b74ea436ae76cdab
Alen Babu <alenbabualen@gmail.com>
2023-11-21 18:50:09 +0530

    crypto live values imported through api, sample interface created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './app.css'
+import axios from 'axios';
+
 
 function App() {
+  const [asset, setAsset] = useState('')
+  const [button, setButton] = useState('')
+
+  const handleInputChange = (e) => {
+    setAsset(e.target.value);
+  };
+
+  const handleButtonclick = (e) => {
+    axios.get(`https://api.blockchain.com/v3/exchange/tickers/${asset}`).then((Response)=>{
+      setButton(Response.data)
+      console.log(button);
+    
+    })};
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="app">
+      <p>Type your crypto asset in capital letters(eg: BTC-USD)</p>
+      <input type='text'  onChange={handleInputChange} placeholder='Type asset name' alt='input' />
+      <button onClick={handleButtonclick}>Submit</button>
+
+      <div>
+        <h2>Symbol:<span>{button.symbol}</span></h2>
+        <h2>Current Price:<span>{button.last_trade_price}</span></h2>
+      </div>
+
     </div>
+
   );
+
 }
 
 export default App;
+
+
